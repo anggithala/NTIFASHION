24b1ed3c5e518b7bc16a01f4f40ef68e6aa4f4f7
anggithala <66930095+anggithala@users.noreply.github.com>
2023-07-04 14:38:24 +0700

    Update cetak.blade.php

--- resources/views/pages/owner/transaksi/cetak.blade.php
+++ resources/views/pages/owner/transaksi/cetak.blade.php
@@ -55,8 +55,13 @@
                     <td style="text-align: right;">{{ number_format($transaction->total_price, 0, ',', '.') }}</td>
                     <td>{{ $transaction->transaction_status }}</td>
                     <td>{{ \Carbon\Carbon::parse($transaction->created_at)->translatedFormat('j F Y H:i') }}</td>
+                    <td>{{ $transaction->sum('total_price') }}</td>
                 </tr>
             @endforeach
+            <tr>
+                <td>Total</td>
+                <td colspan="4">{{ $totalHarga }}</td>
+            </tr>
         </table>
     </main>
 </body>
